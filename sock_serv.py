3e0fbb829ef602bb271b0ea5d0efb5d133ed62a9
Yaroslav <47113131+Yaroher2442@users.noreply.github.com>
2021-04-16 16:18:07 +0300

    v0.3

--- sock_serv.py
+++ sock_serv.py
@@ -1,10 +1,11 @@
 from socket import *
 import queue
 from sock_thr import SockThread
+import asyncio
 
 # данные сервера
-host = '127.0.0.1'
-port = 9090
+host = '0.0.0.0'
+port = 9091
 addr = (host, port)
 
 sock_queue = queue.Queue()
@@ -17,9 +18,10 @@ while True:
     try:
         print('wait connection...')
         conn, addr = input_socket.accept()
+        loop=asyncio.get_event_loop()
         print('client addr: ', addr)
         sock_queue.put((conn, addr))
-        sock_worker = SockThread(sock_queue)
+        sock_worker = SockThread(sock_queue, loop)
         sock_worker.setDaemon(True)
         sock_worker.start()
     except KeyboardInterrupt:
--- sock_thr.py
+++ sock_thr.py
@@ -2,14 +2,24 @@ import threading
 import queue
 from socket import *
 import time
+from pprint import pprint
+from ws_serv import WsThreads
+from serv_2 import hello
+import websockets
+import asyncio
 
+"/server -> clear /server "
+"no /server  redirect -> "
 "http://192.168.3.121:48080/"
+"ws://192.168.3.121:48088/websocket"
+
 
 class SockThread(threading.Thread):
-    def __init__(self, sock_queue):
+    def __init__(self, sock_queue, loop):
         super(SockThread, self).__init__()
         self.sock_queue = sock_queue
         self.conn, self.addr = self.sock_queue.get()
+        self.loop=loop
         print("socket thread up")
 
     def run(self):
@@ -18,18 +28,31 @@ class SockThread(threading.Thread):
             self.conn.close()
             return -1
         else:
-            inner_socket = socket(AF_INET, SOCK_STREAM)
-            if "server" in str(data):
-                inner_socket.connect(("192.168.1.91", 5000))
+            if "Connection: Upgrade" in str(data):
+                # worker = WsThreads(self.conn,self.addr)
+                # worker.setDaemon(True)
+                # worker.start()
+                # worker.join()
+                start_server = websockets.serve(hello, self.addr[0], self.addr[1])
+                self.loop.run_until_complete(start_server)
+                self.loop.run_forever()
+
             else:
-                inner_socket.connect(("127.0.0.1", 5000))
-            inner_socket.send(data)
-            time.sleep(0.001)
-            print(data)
-            response_data = inner_socket.recv(4000)
-            print(response_data)
-            self.conn.send(response_data)
-            self.conn.close()
-            inner_socket.close()
-            print("socket thread close")
-            return 0
+                inner_socket = socket(AF_INET, SOCK_STREAM)
+                if "server" in str(data):
+                    inner_socket.connect(("192.168.3.121", 48080))
+                    buff_ = data.decode('utf-8').replace('/server', '')
+                    data = str.encode(buff_)
+                else:
+                    inner_socket.connect(("127.0.0.1", 5000))
+                inner_socket.send(data)
+                time.sleep(0.5)
+                # print(data)
+                response_data = inner_socket.recv(4000)
+                # print(response_data)
+                pprint({'request': data, 'response': response_data})
+                self.conn.send(response_data)
+                self.conn.close()
+                inner_socket.close()
+                print("socket thread close")
+        return 0
